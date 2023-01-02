1d38d2c00abb7b2121cad6e015c52b84dbf37040
Samuel Camps <samuel.camps@student.ap.be>
2023-01-02 01:11:11 +0100

    toevoegen plug-ins

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,17 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
-  ],
-};
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        /*
+         * De volledige URL van je Headless WordPress site's GraphQL API.
+         * Voorbeeld : "https://www.example-site.com/graphql"
+         */
+        
+        url: "http://expo-85.local/graphql",
+      }
+    }
+  ]
+}
