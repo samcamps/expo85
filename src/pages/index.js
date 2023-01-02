1790c8f8c977dd0459cfa460916ba628270bab40
Samuel Camps <samuel.camps@student.ap.be>
2023-01-02 02:09:00 +0100

    homePagequery ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,50 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../components/layout'
 
-const IndexPage = () => {
+const IndexPage = ({
+  data: {
+    wpPage: {
+      homePage
+    }
+  }
+}) => {
+
+  const image = getImage(homePage.picture.localFile)
 
   return (
     <Layout>
-      
-      <h1>Welcome to expo 85</h1>
-      <p>Lorem ipsum</p>
+
+      <h1>{homePage.title}</h1>
+
+      <div dangerouslySetInnerHTML={{ __html: homePage.description }} />
+
+      <GatsbyImage image={image} alt={homePage.picture.altText} />
+
     </Layout>
   )
 }
 
 
 export default IndexPage
+
+export const query = graphql`
+
+query  {
+  wpPage(slug: {eq: "home-page"}) {
+    homePage {
+      title
+      description
+      picture {
+        localFile {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED,transformOptions: {grayscale: true})
+          }
+        }
+        altText
+      }
+    }
+  }
+}
+`
