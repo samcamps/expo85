533baabbf95cc8bac24932bf4d8cb83f921b4988
Samuel Camps <samuel.camps@student.ap.be>
2023-01-05 14:18:20 +0100

    artworkpage ok

--- src/pages/artworks/index.js
+++ src/pages/artworks/index.js
@@ -6,6 +6,7 @@ const ArtworksPage = ({
   data: {
     allWpArtwork: {
       edges
+      
     }
   }
 }) => {
---
+++ src/pages/artworks/{wpArtwork.slug}.js
@@ -0,0 +1,63 @@
+
+import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import * as React from 'react'
+import Layout from '../../components/layout'
+
+
+const ArtworkPage = ({
+    data: {
+        wpArtwork: {
+            artworkMeta: el
+        }
+    }
+}) => {
+
+    const image = getImage(el.picture.localFile)
+
+    return (
+
+        <Layout >
+            <div>
+                <h1>{el.title}</h1>
+                <h3>{el.artist}</h3>
+                <div dangerouslySetInnerHTML={{ __html: el.description }} />
+                <p>Year: {el.year}</p>
+                <p>Type: {el.type}</p>
+                <p>Signed: {el.signed}</p>
+                <GatsbyImage image={image} alt={el.picture.altText} />
+
+
+            </div>
+        </Layout>
+    )
+}
+
+
+
+export default ArtworkPage
+
+
+export const query = graphql`
+
+ query ($id: String) {
+    wpArtwork(id: {eq: $id}) {
+      artworkMeta {
+        artist
+        description
+        signed
+        title
+        type
+        year
+        picture {
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED)
+            }
+          }
+        }
+      }
+    }
+  }
+  `
