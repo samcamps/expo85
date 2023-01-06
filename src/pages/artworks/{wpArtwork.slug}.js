ee1c24fce0185bd3594528cbca09b0019455ba3f
Samuel Camps <samuel.camps@student.ap.be>
2023-01-06 17:39:42 +0100

    css detailpage

--- src/pages/artworks/{wpArtwork.slug}.js
+++ src/pages/artworks/{wpArtwork.slug}.js
@@ -1,8 +1,15 @@
-
 import { graphql } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 import Layout from '../../components/layout'
+import {
+    title,
+    description,
+    properties,
+    artHero,
+    pictureContainer,
+    artistName
+} from '../pages.module.css'
 
 
 const ArtworkPage = ({
@@ -22,23 +29,33 @@ const ArtworkPage = ({
     return (
 
         <Layout >
-            <div>
-                <h2>{el.title},  {el.year} </h2>
-                <h3>{el.artist}</h3>
+            <div className={description}>
+                <h2 className={title}>{el.title},  {el.year} </h2>
+                <hr></hr>
+                <h3 className={artistName}>{el.artist}</h3>
                 <div dangerouslySetInnerHTML={{ __html: el.description }} />
-                <p>Afmetingen: {el.dimensions}</p>
-                <p>Type: {el.type}</p>
-                {el.signed && <p>Gesigneerd</p>}
+                <hr></hr>
 
-                <div><p>Provenance: 
-                    {nodes.map((el, index) => (
-                        <span key={index}>
-                           {" "} {el.name} {index + 1 < nodes.length && "- "}
-                        </span>
-                    ))}
-                </p></div>
-                <GatsbyImage image={image} alt={el.picture.altText} />
+                <div className={pictureContainer}>
+                    <GatsbyImage className={artHero} image={image} alt={el.picture.altText} />
+                  
+                </div>
+                <div className={properties}>
+                    <p>Afmetingen: {el.dimensions}</p>
+                    <p>Type: {el.type}</p>
+                    {el.signed && <p>Gesigneerd</p>}
 
+                    <div>
+                        <p>Provenance:
+                            {nodes.map((el, index) => (
+                                <span key={index}>
+                                    {" "} {el.name} {index + 1 < nodes.length && "- "}
+                                </span>
+                            ))}
+                        </p>
+                    </div>
+                </div>
+                <hr></hr>
 
             </div>
         </Layout>
